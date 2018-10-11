6971ff9cac76ff2a58c64455ca6b6674e75ff178
Matt Kerbel <mkerb003@fiu.edu>
2018-10-11 01:24:33 -0400

    Additional method
    Added a method to format output

--- src/autoDealership/DriverClass.java
+++ src/autoDealership/DriverClass.java
@@ -2,20 +2,50 @@ package autoDealership;
 import autoDealership.VehicleTypes.*;
 import autoDealership.TrimLevels.*;
 import autoDealership.SoloOptions.*;
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Iterator;
 /**
  *
  * @author mkmat
  */
 public class DriverClass {
+  
+    public DriverClass() {
+        
+    }
     
     public static void main(String args[]) {
+        
+        DriverClass driver = new DriverClass();
 
         Vehicles baseSportsCar = new SportsCar();
         
-        System.out.println("Vehicle: " + baseSportsCar.getDesc());
-        System.out.println("Cost: " + baseSportsCar.cost());
-        System.out.println("Options: " + baseSportsCar.getOptions());
+        System.out.println(driver.formatOptions(baseSportsCar));
+        
+        Vehicles raceCar = new SportsCar();
+        raceCar = new Track(raceCar);
+        
+        System.out.println(driver.formatOptions(raceCar));
    
+    }
+    
+    public String formatOptions(Vehicles vehicle) {
+        
+        DecimalFormat df = new DecimalFormat("$#,###.00");
+        HashMap<String, String> options = vehicle.getOptions();
+        Iterator<HashMap.Entry<String, String>> entries = options.entrySet().iterator();
+        String result = "";
+        
+        result += ("Vehicle: " + vehicle.getDesc() + "\n" + "Cost: " + df.format(vehicle.cost()) + "\n" + "Options:\n");
+        
+        while (entries.hasNext()) {
+            
+            HashMap.Entry<String, String> entry = entries.next();
+            result += ("   " + entry.getKey() + ": " + entry.getValue() + "\n");
+        }
+        
+        return result;
     }
     
 }
